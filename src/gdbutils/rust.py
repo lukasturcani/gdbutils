fd38b1d50f4436aa85bf56881a85ee16878a6ad9
Lukas Turcani <lukasturcani@mailbox.org>
2022-06-29 09:13:31 +0100

    Add container operations to `rust.Vector`

--- src/gdbutils/rust.py
+++ src/gdbutils/rust.py
@@ -25,7 +25,22 @@ class Vector:
         return int(self.vector["len"])
 
     def __iter__(self) -> typing.Iterator[gdb.Value]:
-        start = self.vector["buf"]["ptr"]["pointer"]
+        pointer = self._start_pointer()
         for _ in range(len(self)):
-            yield start.referenced_value()
-            start += 1
+            yield pointer.referenced_value()
+            pointer += 1
+
+    def __reversed__(self) -> typing.Iterator[gdb.Value]:
+        pointer = self._start_pointer() + len(self) - 1
+        for _ in range(len(self)):
+            yield pointer.referenced_value()
+            pointer -= 1
+
+    def __getitem__(self, index: int) -> gdb.Value:
+        if index >= len(self):
+            raise IndexError("Index out of range.")
+        pointer = self._start_pointer() + index
+        return pointer.referenced_value()
+
+    def _start_pointer(self) -> gdb.Value:
+        return self.vector["buf"]["ptr"]["pointer"]
